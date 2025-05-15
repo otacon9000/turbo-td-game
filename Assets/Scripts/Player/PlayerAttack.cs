fbb86fdeff25ff2e7d1e6388edbae976698ea467
Andrea <andrea.alicino@gmail.com>
2025-05-14 20:42:21 -0400

    add melee hitbox prefab and logic

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,17 +1,22 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using JetBrains.Annotations;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Rendering;
+
 
 public class PlayerAttack : MonoBehaviour
 {
+    [Header("Fire Attack")]
     public GameObject bulletPrefab;
     [SerializeField] private Transform _bulletSpawn;
-    [SerializeField] private float _attackCooldown = 0.2f;
+    [SerializeField] private float _attackFireCooldown = 0.2f;
 
+    [Header("Melee Attack")]
+    public GameObject meleeHitboxPrefab;
+    [SerializeField] private Transform _meleePoint;
+    [SerializeField] private float _attackMeleeCooldown = 0.5f;
+
+    private bool canAttack = true;
+    
     private Camera _mainCamera;
     private float _lastAttackTime;
     private PlayerInputAction _inputAction;
@@ -25,18 +30,20 @@ public class PlayerAttack : MonoBehaviour
     private void OnEnable()
     {
         _inputAction.Player.Enable();
-        _inputAction.Player.Attack.performed += OnAttack;
+        _inputAction.Player.Attack.performed += OnAttackFire;
+        _inputAction.Player.Attack.performed += OnAttackMelee;
     }
 
     private void OnDisable()
     {
-        _inputAction.Player.Attack.performed -= OnAttack; 
+        _inputAction.Player.Attack.performed -= OnAttackFire; 
+        _inputAction.Player.Attack.performed -= OnAttackMelee; 
         _inputAction.Player.Disable();
     }
 
-    private void OnAttack(InputAction.CallbackContext context)
+    private void OnAttackFire(InputAction.CallbackContext context)
     {
-        if (Time.time - _lastAttackTime < _attackCooldown) return;
+        if (Time.time - _lastAttackTime < _attackFireCooldown) return;
         
         Vector2 mousePoint = _mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         Vector2 shootDirection = mousePoint - (Vector2)_bulletSpawn.position;
@@ -46,6 +53,20 @@ public class PlayerAttack : MonoBehaviour
         bullet.GetComponent<Bullet>().Initialize(shootDirection);
 
         _lastAttackTime = Time.time;
+    }
+    
+    private void OnAttackMelee(InputAction.CallbackContext context)
+    {
+        if (!canAttack) return;
+
+        GameObject hitbox = Instantiate(meleeHitboxPrefab, _meleePoint.position, _meleePoint.rotation);
+        canAttack = false;
+        Invoke(nameof(ResetAttack), _attackMeleeCooldown);
+    }
+    
+    private void ResetAttack()
+    {
+        canAttack = true;
     }
     
 }
